808fb7649a7d2d475edbc77f77c96eed14e283b9
gchaperon <gabrielchaperonb@gmail.com>
2022-02-18 12:50:06 -0300

    add pre-commit stuff

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+def nfibonacci(n: int) -> list[int]:
+    print(os.name)
+    """Returns the n first fib numbers, n=0 -> [], n=1 -> [0], n=2 -> [0, 1], ..."""
+    s = [0, 1]
+    for _ in range(2, n):
+        s.append(s[-1] + s[-2])
+    return s[:n]
+
+
+def fibonacci(n: int) -> int:
+    """Returns nth fib number, fib_0 = 0, fib_1 = 1, ..."""
+    print(sys.platform)
+    return nfibonacci(n + 1)[-1]
